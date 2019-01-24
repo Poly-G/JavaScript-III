ebc8a8552ddbcb1d941a18c36d2fd16c76d615a3
Poly Goveia <poly.goveia@gmail.com>
2019-01-24 17:09:20 -0500

    add this.js examples of binding

--- assignments/this.js
+++ assignments/this.js
@@ -12,15 +12,45 @@
 // Principle 1
 
 // code example for Window Binding
+this.name = 'Poly';
 
 // Principle 2
 
 // code example for Implicit Binding
+const animal = {
+    type: 'frog',
+    name: 'Ted',
+    age: 2,
+    speak: function () {
+        console.log(`Hello, my name is ${this.name}`);
+    }
+}
+
+animal.speak();
 
 // Principle 3
 
 // code example for New Binding
+function Car (attribute) {
+    this.make = attribute.make;
+    this.year = attribute.year
+}
+
+const honda = new Car({
+    make: 'Honda',
+    year: 2006
+})
 
 // Principle 4
 
 // code example for Explicit Binding
+const person = {
+    name: 'Poly',
+    age: 26
+}
+
+function intro (){
+    console.log(`Hi, my name is ${this.name}`)
+}
+
+intro.call(person);
