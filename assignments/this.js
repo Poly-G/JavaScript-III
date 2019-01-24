690539f365f874ad6f5974fe95c6c13859c5d4ac
Poly Goveia <poly.goveia@gmail.com>
2019-01-24 16:17:15 -0500

    initial commit

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window binding
+* 2. New binding
+* 3. Implicit binding
+* 4. Explicit binding
 *
 * write out a code example of each explanation above
 */
