fc5e8131a42e57e16f211c69e4b3c234ceefa612
Poly Goveia <poly.goveia@gmail.com>
2019-01-24 16:49:35 -0500

    add this.js explanations for this

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. Window binding
-* 2. New binding
-* 3. Implicit binding
-* 4. Explicit binding
+* 1. Window binding - default binding to the window object in the browser if no other binding is used. 
+* 2. New binding - Keyword new is used when building out constructor functions
+* 3. Implicit binding - Dot notation to invoke a function, whatever is to the left of the dot becomes the context for 'this'.
+* 4. Explicit binding - When .call(), .apply(), and .bind() are used on a function.
 *
 * write out a code example of each explanation above
 */
